8e264edf9e19580b4d8bfadfca0b6245e69a632d
Kevin Lam <klam@lcs.on.ca>
2025-02-24 15:07:25 -0500

    Initial Commit

---
+++ ComputedNumbersRevisited/ComputedNumbersRevisitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputedNumbersRevisitedApp.swift
+//  ComputedNumbersRevisited
+//
+//  Created by Kevin Lam on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ComputedNumbersRevisitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComputedNumbersRevisited/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComputedNumbersRevisited
+//
+//  Created by Kevin Lam on 24/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
